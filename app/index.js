44c6aa4842920e3fb11f1e981e36036cfb98e997
thomas.chatel <thomas.chatel.acversailles@gmail.com>
2022-03-15 11:52:31 +0100

    Add back skeleton with express

---
+++ app/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.status(200).send('ok');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.info("back listening on port " + PORT));
