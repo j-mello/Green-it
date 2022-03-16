6058766807c46b6d9ea3466c5ef5bac45bfb2790
Julien BOUVET <julienbouvet78@hotmail.com>
2022-03-16 15:50:45 +0100

    trying to resolve tome bugs while aggregating csv in database

--- app/generateDatabase.js
+++ app/generateDatabase.js
@@ -1,42 +1,34 @@
 const fs = require("fs");
-const fsPromise = require("fs/promises");
 const indicesConfig = require("./indicesConfig");
 const City = require("./Models/City");
 
-const createCityGetter = async (callback) => {
-    const value = await callback();
-    return (callback) => {
-        callback(value);
-    }
-}
-
 async function generateDatabase() {
     await City.deleteMany({});
+    console.log("database purged");
 
     const computingPromises = [];
-    const globalCityGetPromises = {};
 
     const path = __dirname+"/docs/";
-    for (const dir of await fsPromise.readdir(path)) {
-        const indices = [];
+    for (const [dir,files] of Object.entries(indicesConfig.foldersAndFiles)) {
+        const indices = {};
 
         const colNamesConfig = indicesConfig.colsNameByFolder[dir] ?? indicesConfig.colsNameByFolder.default;
 
-        let cols = [colNamesConfig.numCommune, colNamesConfig.nameCommune, 'Département'];
+        let cols = [colNamesConfig.codeCommune, colNamesConfig.nameCommune, 'Département'];
         for (const axe of indicesConfig.axes) {
             for (const indice of axe.indices) {
                 if (indice.docFolder === dir) {
-                    indices.push({
+                    indices[indice.name] = {
                         ...indice,
                         axeName: axe.name
-                    });
+                    }
                     cols = [...cols, ...indice.cols];
                 }
             }
         }
 
-        for (const file of await fsPromise.readdir(path+dir)) {
-            computingPromises.push((() => new Promise(resolve => {
+        for (const file of files) {
+            await (() => new Promise(resolve => {
                 console.log("read "+file);
                 const stream = fs.createReadStream(path+dir+"/"+file, {encoding: 'utf8'});
                 let city = null;
@@ -46,21 +38,12 @@ async function generateDatabase() {
                 const indexByColName = {};
                 let i = 0;
 
-                stream.on('end', async () => {
-                    console.log("finished "+file);
-                    if (city) {
-                        try {
-                            await city.save();
-                        } catch(e) {
-                            console.log("save failed");
-                            console.log(city);
-                            throw e;
-                        }
-                    }
-                    resolve();
-                })
-
                 const eachLine = async data => {
+                    if (data.trim() === '')
+                        return;
+                    if (i%500 === 0) {
+                        console.log("line "+i);
+                    }
                     if (i === 0) {
                         for (const [i,colname] of Object.entries(data.split(","))) {
                             if (cols.includes(colname)) {
@@ -76,8 +59,18 @@ async function generateDatabase() {
                     }
 
                     const line = data.split(",");
-                    if (city === null || city.codeCommune !== line[indexByColName[colNamesConfig.numCommune]]) {
+                    if (city === null || city.codeCommune !== line[indexByColName[colNamesConfig.codeCommune]]) {
                         if (city !== null) {
+                            for (const axe of city.axes) {
+                                for (const indice of axe.indices) {
+                                    //console.log(indice);
+                                    //console.log(indices);
+                                    //console.log(indices[indice.nom]);
+                                    if (indices[indice.nom] && indice.value === null) {
+                                        indice.value = indices[indice.nom].aggregate(indice.set);
+                                    }
+                                }
+                            }
                             try {
                                 await city.save();
                             } catch(e) {
@@ -88,33 +81,28 @@ async function generateDatabase() {
                         }
                         axesByName = {};
                         indicesByNameAndAxeName = {};
-                        if (globalCityGetPromises[line[indexByColName[colNamesConfig.numCommune]]] === undefined) {
-                            globalCityGetPromises[line[indexByColName[colNamesConfig.numCommune]]] = createCityGetter(() => City.findOne({codeCommune: line[indexByColName[colNamesConfig.numCommune]]}));
-                        }
-                        city = await (() => new Promise(resolve => {
-                            globalCityGetPromises[line[indexByColName[colNamesConfig.numCommune]]](resolve)
-                        }));
-                        delete globalCityGetPromises[line[indexByColName[colNamesConfig.numCommune]]];
 
+                        const obj = {codeCommune: parseInt(line[indexByColName[colNamesConfig.codeCommune]])};
+                        //console.log("find =>");
+                        //console.log(obj);
+                        city = await City.findOne(obj);
                         if (city === null) {
                             const obj = {
                                 nom: line[indexByColName[colNamesConfig.nameCommune]],
-                                codeCommune: line[indexByColName[colNamesConfig.numCommune]],
+                                codeCommune: parseInt(line[indexByColName[colNamesConfig.codeCommune]]),
                                 codeDepartement: line[indexByColName['Département']],
                                 axes: []
                             };
                             try {
-                                console.log("create city "+obj.nom+" ("+obj.codeCommune+")");
-                                globalCityGetPromises[obj.codeCommune] = createCityGetter(() => City.create(obj));
-                                city = await (() => new Promise(resolve => {
-                                    globalCityGetPromises[line[indexByColName[colNamesConfig.numCommune]]](resolve)
-                                }));
-                                delete globalCityGetPromises[obj.codeCommune];
+                                //console.log("create city "+obj.nom+" ("+obj.codeCommune+")");
+                                //console.log(obj);
+                                city = await City.create(obj);
+                                //console.log("created")
                             } catch (e) {
                                 console.log({file});
                                 console.log("create failed");
                                 console.log(obj);
-                                console.log("cols =>");
+                                console.log("cols =>");{codeCommune: parseInt(line[indexByColName[colNamesConfig.codeCommune]])}
                                 console.log(cols);
                                 console.log({i})
                                 console.log("line =>");
@@ -126,31 +114,58 @@ async function generateDatabase() {
                         }
                     }
 
-                    for (const indice of indices) {
+                    for (const indice of Object.values(indices)) {
                         if (axesByName[indice.axeName] === undefined) {
-                            city.axes.push({
-                                name: indice.axeName,
-                                indices: []
-                            });
-                            axesByName[indice.axeName] = city.axes[city.axes.length-1];
+                            axesByName[indice.axeName] = city.axes.find(axe => axe.nom === indice.axeName);
+                            if (axesByName[indice.axeName] === undefined) {
+                                city.axes.push({
+                                    nom: indice.axeName,
+                                    indices: []
+                                });
+                                axesByName[indice.axeName] = city.axes[city.axes.length - 1];
+                            }
                         }
                         const axe = axesByName[indice.axeName];
                         if (indicesByNameAndAxeName[axe.name] === undefined) {
                             indicesByNameAndAxeName[axe.name] = {};
                         }
                         if (indicesByNameAndAxeName[axe.name][indice.name] === undefined) {
-                            axe.indices.push({
-                                name: indice.name,
-                                value: 0
-                            });
-                            indicesByNameAndAxeName[axe.name][indice.name] = axe.indices[axe.indices.length-1];
+                            indicesByNameAndAxeName[axe.name][indice.name] = axe.indices.find(eachIndice => eachIndice.nom === indice.name);
+                            if (indicesByNameAndAxeName[axe.name][indice.name] === undefined) {
+                                axe.indices.push({
+                                    nom: indice.name,
+                                    value: null,
+                                    set: []
+                                });
+                                indicesByNameAndAxeName[axe.name][indice.name] = axe.indices[axe.indices.length - 1];
+                            }
                         }
                         for (const col of indice.cols) {
-                            indicesByNameAndAxeName[axe.name][indice.name].value += parseInt(line[indexByColName[col]]);
+                            const n = parseFloat(line[indexByColName[col]]);
+                            if (!isNaN(n)) {
+                                indicesByNameAndAxeName[axe.name][indice.name].set.push(n);
+                            }
                         }
                     }
                     i ++;
                 }
+
+                const onClose = async () => {
+                    console.log("finished "+file);
+                    if (city) {
+                        try {
+                            await city.save();
+                        } catch(e) {
+                            console.log("save failed");
+                            console.log(city);
+                            throw e;
+                        }
+                    }
+                    resolve();
+                }
+
+                let finished = false;
+                let paused = false;
 
                 let currentLines = "";
                 stream.on('data', async data => {
@@ -159,12 +174,25 @@ async function generateDatabase() {
                     if ((lines = currentLines.split("\n")).length > 1) {
                         currentLines = lines[lines.length-1];
                         lines = lines.slice(0,-1);
+                        stream.pause();
+                        paused = true;
                         for (const line of lines) {
-                            eachLine(line)
+                            await eachLine(line);
                         }
+                        stream.resume();
+                        paused = false;
+                        if (finished)
+                            onClose();
                     }
                 });
-            }))());
+
+                stream.on('end', async () => {
+                    await eachLine(currentLines);
+                    finished = true;
+                    if (!paused)
+                        onClose();
+                })
+            }))();
         }
     }
 
--- app/indicesConfig.js
+++ app/indicesConfig.js
@@ -1,11 +1,35 @@
+const sum = values => values.reduce((acc,value) => acc+value, 0)
+const average = values => sum(values)/values.length;
+
+
 module.exports = {
+    foldersAndFiles: {
+        "Couples - Familles - Ménages en 2016": [
+            "base-ic-couples-familles-menages-2016.csv",
+            "base-ic-couples-familles-menages-2016-com.csv"
+        ],
+        "Diplômes - Formation en 2016": [
+            "base-ic-diplomes-formation-2016.csv",
+            "base-ic-diplomes-formation-2016-com.csv"
+        ],
+        "Population 2016": [
+            "base-ic-evol-struct-pop-2016.csv",
+            "base-ic-evol-struct-pop-2016-com.csv"
+        ],
+        "Revenus, pauvreté et niveau de vie en 2015 (IRIS)": [
+            "BASE_TD_FILO_DEC_IRIS_2015.csv"
+        ],
+        "Revenus et pauvreté des ménages en 2016": [
+            "base-cc-filosofi-2016.csv"
+        ]
+    },
     colsNameByFolder: {
         default: {
-            numCommune: 'Commune ou ARM',
+            codeCommune: 'Commune ou ARM',
             nameCommune: 'Libellé commune ou ARM',
         },
         "Revenus et pauvreté des ménages en 2016": {
-            numCommune: 'Code géographique',
+            codeCommune: 'Code géographique',
             nameCommune: 'Libellé géographique'
         }
     },
@@ -16,12 +40,14 @@ module.exports = {
                 {
                     name: "Part des ménages d’une personne",
                     docFolder: "Couples - Familles - Ménages en 2016",
-                    cols: ["Ménages 1 personne en 2016 (compl)"]
+                    cols: ["Ménages 1 personne en 2016 (compl)"],
+                    aggregate: sum
                 },
                 {
                     name: "Part des ménages monoparentaux",
                     docFolder: "Couples - Familles - Ménages en 2016",
-                    cols: ["Pop mén fam princ Famille mono en 2016 (compl)"]
+                    cols: ["Pop mén fam princ Famille mono en 2016 (compl)"],
+                    aggregate: sum
                 }
             ]
         },
@@ -31,22 +57,26 @@ module.exports = {
                 {
                     name: "Part des chômeurs (15 – 64 ans)",
                     docFolder: "Revenus, pauvreté et niveau de vie en 2015 (IRIS)",
-                    cols: ["dont part des indemnités de chômage (%)"]
+                    cols: ["dont part des indemnités de chômage (%)"],
+                    aggregate: average
                 },
                 {
                     name: "Taux de pauvreté",
                     docFolder: "Revenus et pauvreté des ménages en 2016",
-                    cols: ["Médiane du niveau vie (€)"]
+                    cols: ["Médiane du niveau vie (€)"],
+                    aggregate: average
                 },
                 {
                     name: "Part des bénéficiaires des minima sociaux",
                     docFolder: "Revenus et pauvreté des ménages en 2016",
-                    cols: ["dont part des minima sociaux (%)"]
+                    cols: ["dont part des minima sociaux (%)"],
+                    aggregate: average
                 },
                 {
                     name: "Part des moins de 25 ans",
                     docFolder: "Population 2016",
-                    cols: ["Pop 18-24 ans en 2016 (princ)"]
+                    cols: ["Pop 18-24 ans en 2016 (princ)"],
+                    aggregate: sum
                 }
             ]
         },
@@ -59,7 +89,8 @@ module.exports = {
                     cols: [
                         'Pop 65-79 ans en 2016 (princ)',
                         'Pop 80 ans ou plus en 2016 (princ)'
-                    ]
+                    ],
+                    aggregate: sum
                 },
                 {
                     name: "Part des personnes de 15 ans\n" +
@@ -72,7 +103,8 @@ module.exports = {
                         "Pop 15 ans ou plus non scol. CAP-BEP en 2016 (princ)",
                         "Pop 15 ans ou plus non scol. BAC en 2016 (princ)",
                         "Pop 15 ans ou plus non scol. Enseignement sup en 2016 (princ)"
-                    ]
+                    ],
+                    aggregate: sum
                 }
             ]
         }
