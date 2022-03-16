8f5f4d3effb4c353e0dbaea259e0a1c3f656cf39
j-mello <mello.julien@gmail.com>
2022-03-16 13:49:42 +0100

    Add milligram

--- app/index.js
+++ app/index.js
@@ -10,9 +10,11 @@ app.use(express.json());
 
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/get200', (req, res) => {
     res.status(200).send('ok');
 });
+
+app.use(express.static('front'))
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.info("back listening on port " + PORT));
